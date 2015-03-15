f4a0b6ddb0e8db22f0108e0dd7743802e6cd2c09
QWp6t <hi@qwp6t.me>
2015-03-14 19:35:19 -0700

    Add options for Google Analytics

--- modules/google-analytics.php
+++ modules/google-analytics.php
@@ -7,27 +7,26 @@ namespace Roots\Soil\GoogleAnalytics;
  *
  * Cookie domain is 'auto' configured. See: http://goo.gl/VUCHKM
  * You can enable/disable this feature in functions.php (or lib/config.php if you're using Sage):
- * add_theme_support('soil-google-analytics');
- * define('GOOGLE_ANALYTICS_ID', 'UA-XXXXXX');
+ * add_theme_support('soil-google-analytics', 'UA-XXXXX-Y', 'wp_footer');
  */
-function google_analytics() {
-  if (!defined('GOOGLE_ANALYTICS_ID') || !GOOGLE_ANALYTICS_ID) {
+list($ga_id, $hook) = $options + ['','wp_footer'];
+add_action($hook, function () use ($ga_id) {
+  if (!$ga_id) {
     return;
   }
-  $displayGA = (!defined('WP_ENV') || WP_ENV === 'production') && !current_user_can('manage_options');
+  $dummyGA = (defined('WP_ENV') && WP_ENV !== 'production') || current_user_can('manage_options');
   ?>
-  <script>
-    <?php if (apply_filters('soil/displayGA', $displayGA)) : ?>
-      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-        e.src='//www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    <?php else : ?>
+  <script> 
+    <?php if (apply_filters('soil/dummyGA', $dummyGA)) : ?>
       function ga() {if (window.console) {console.log('Google Analytics: ' + [].slice.call(arguments));}}
+    <?php else : ?>
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      e.src='//www.google-analytics.com/analytics.js';
+      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
     <?php endif; ?>
-      ga('create','<?= GOOGLE_ANALYTICS_ID; ?>','auto');ga('send','pageview');
+    ga('create','<?= $ga_id; ?>','auto');ga('send','pageview');
   </script>
-  <?php
-}
-add_action('wp_footer', __NAMESPACE__ . '\\google_analytics', 20);
+<?php
+}, 20);
